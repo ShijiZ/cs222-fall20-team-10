a8fa6fe124d75986a1226a8ac836add0d42d601b
郝大海 <haodahai@dhcp-10-8-028-255.mobile.reshsg.uci.edu>
2020-10-15 12:41:31 -0700

    test

--- src/rbfm/rbfm.cc
+++ src/rbfm/rbfm.cc
@@ -60,6 +60,7 @@ namespace PeterDB {
 
     RC RecordBasedFileManager::readRecord(FileHandle &fileHandle, const std::vector<Attribute> &recordDescriptor,
                                           const RID &rid, void *data) {
+        dahai;
         return -1;
     }
 
