10e903b949cab94b7dd4aa1f58c0d552992c1ceb
Shiji Zhao <zhaoshiji2016@gmail.com>
2020-10-15 18:24:53 -0700

    Continue on rbfm

--- src/rbfm/rbfm.cc
+++ src/rbfm/rbfm.cc
@@ -40,24 +40,40 @@ namespace PeterDB {
         unsigned recordLength = getRecordLength(recordDescriptor, data);
         unsigned bytesNeeded = recordLength + 2*sizeof(short);
 
-        unsigned pageNum = fileHandle.getNumberOfPages();
-        if (pageNum == 0) {
-            // TODO: Shiji
-        }
-        else {
-            unsigned short freeBytes = getFreeBytes();
-            if (freeBytes < bytesNeeded) {
-                // TODO: For LOOP
-            }
-        }
-        
         void* recordBuffer = malloc(recordLength);
         generateRecord(recordDescriptor, data, recordBuffer);
-        insertRecordData();
+
+        void* pageBuffer = malloc(PAGE_SIZE);
+        unsigned pageNum = fileHandle.getNumberOfPages();
+        bool recordInserted = false;
+        if (pageNum > 0) {
+            fileHandle.readPage(pageNum, pageBuffer);
+            unsigned short freeBytes = getFreeBytes(pageBuffer);
+            if (freeBytes >= bytesNeeded) {
+                recordInserted = insertRecordToPage(recordBuffer, pageBuffer);
+            }
+            else {
+                for (int pageIndex = 0; pageIndex < pageNum-1; pageIndex++) {
+                    fileHandle.readPage(pageNum, pageBuffer);
+                    freeBytes = getFreeBytes(pageBuffer);
+                    if (freeBytes >= bytesNeeded) {
+                        recordInserted = insertRecordToPage(recordBuffer, pageBuffer);
+                        break;
+                    }
+                }
+            }
+        }
+        if (!recordInserted) {
+            initNewPage(recordBuffer, pageBuffer);
+            fileHandle.appendPage(pageBuffer);
+        }
+        free(recordBuffer);
+
         insertSlot();
         updateSlotsNumAndFreeBytes();
 
-        fileHandle.writePage(PageNum, pageBuffer);
+        fileHandle.writePage(pageNum-1, pageBuffer);
+        free(pageBuffer);
         return 0;
     }
 
@@ -99,15 +115,16 @@ namespace PeterDB {
     /*****    Helper functions  *******/
     /**********************************/
     unsigned RecordBasedFileManager::getRecordLength(const std::vector<Attribute> &recordDescriptor, const void *data) {
-        const int attrNum = recordDescriptor.size();
+        const unsigned attrNum = recordDescriptor.size();
         int nullInfoByte = ceil(((double) attrNum)/8);
         char* buffer = new char[nullInfoByte];
         std::memcpy(buffer, data, nullInfoByte);
 
         unsigned recordLength = 0;
         char* attrPtr = (char*) data + nullInfoByte;
-        for (int byteIndex = 0; byteIndex < attrNum; byteIndex++) {
-            for (int bitIndex = 0; bitIndex < 8; bitIndex++) {
+        unsigned attrCounter = 0;
+        for (int byteIndex = 0; byteIndex < nullInfoByte; byteIndex++) {
+            for (int bitIndex = 0; bitIndex < 8 && attrCounter < attrNum; bitIndex++) {
                 bool isNull = buffer[byteIndex] & (short) 1 << (short) (7 - bitIndex);
                 if (!isNull) {
                     Attribute attr = recordDescriptor[byteIndex*8 + bitIndex];
@@ -123,10 +140,107 @@ namespace PeterDB {
                         attrPtr += sizeof(unsigned);
                     }
                 }
+                attrCounter++;
             }
         }
         delete[] buffer;
         return recordLength;
+    }
+
+    void RecordBasedFileManager::generateRecord(const std::vector<Attribute> &recordDescriptor, const void *data,
+                                                void *recordBuffer) {
+        // Write attrNum
+        const unsigned attrNum = recordDescriptor.size();
+        std::memcpy(recordBuffer, &attrNum, sizeof(unsigned));
+
+        // Get null info bytes
+        unsigned nullInfoByte = ceil(((double) attrNum)/8);
+        char* nullInfoBuffer = new char[nullInfoByte];
+        std::memcpy(nullInfoBuffer, data, nullInfoByte);
+
+        // Create required pointers
+        int* offsetDirPtr = (int*) recordBuffer + 1;
+        char* attrWritePtr = (char*) recordBuffer + (1+attrNum)*sizeof(unsigned);
+        char* attrReadPtr = (char*) data + nullInfoByte;
+
+        unsigned attrCounter = 0;
+        unsigned offset = 0;
+        for (int byteIndex = 0; byteIndex < nullInfoByte; byteIndex++) {
+            for (int bitIndex = 0; bitIndex < 8 && attrCounter < attrNum; bitIndex++) {
+                // Determine if attribute is null
+                bool attrIsNull = nullInfoBuffer[byteIndex] & (short) 1 << (short) (7 - bitIndex);
+                if (!attrIsNull) {
+                    Attribute attr = recordDescriptor[byteIndex*8 + bitIndex];
+                    AttrType attrType = attr.type;
+
+                    // Attribute is of type varChar
+                    if (attrType == TypeVarChar) {
+                        // get varChar length
+                        unsigned varCharLen = 0;
+                        memcpy(&varCharLen, attrReadPtr, sizeof(unsigned));
+                        attrReadPtr += sizeof(unsigned);
+
+                        // write offset info to offsetDir
+                        offset += varCharLen;
+                        memcpy(offsetDirPtr, &offset, sizeof(unsigned));
+                        offsetDirPtr += 1;
+
+                        // write length info and attribute to attribute region
+                        memcpy(attrWritePtr, &varCharLen, sizeof(unsigned));
+                        memcpy(attrWritePtr, attrReadPtr, varCharLen);
+                        attrWritePtr += sizeof(unsigned) + varCharLen;
+                        attrReadPtr += varCharLen;
+                    }
+                    // Attribute is of type int or real
+                    else {
+                        // write offset info to offsetDir
+                        offset += 4;
+                        memset(offsetDirPtr, offset, sizeof(unsigned));
+                        offsetDirPtr += 1;
+
+                        // write attribute to attribute region
+                        memcpy(attrWritePtr, attrReadPtr, 4);
+                        attrWritePtr += 4;
+                        attrReadPtr += 4;
+                    }
+                }
+                // Attribute is null
+                else {
+                    // write offset info to offsetDir
+                    memset(offsetDirPtr, -1, sizeof(int));
+                    offsetDirPtr += 1;
+                }
+                attrCounter++;
+            }
+        }
+        delete[] nullInfoBuffer;
+    }
+
+    unsigned short RecordBasedFileManager::getSlotsNum(void* pageBuffer) {
+        unsigned short slotsNum;
+        memcpy(&slotsNum, (char*) pageBuffer+PAGE_SIZE-2*sizeof(unsigned short), sizeof(unsigned short));
+        return slotsNum;
+    };
+
+    unsigned short RecordBasedFileManager::getFreeBytes(void* pageBuffer) {
+        unsigned short freeBytes;
+        memcpy(&freeBytes, (char*) pageBuffer+PAGE_SIZE-sizeof(unsigned short), sizeof(unsigned short));
+        return freeBytes;
+    };
+
+    unsigned short RecordBasedFileManager::getInsertStartOffset(void* pageBuffer) {
+        unsigned short slotsNum = getSlotsNum(pageBuffer);
+        unsigned short insertStartOffset;
+        unsigned short lastRecordLen;
+        unsigned short lastRecordLen;
+        memcpy(&insertStartOffset, (char*) pageBuffer+PAGE_SIZE-(1+slotsNum*2)*sizeof(unsigned short), sizeof(unsigned short));
+        return insertStartOffset;
+    };
+
+    bool RecordBasedFileManager::insertRecordToPage(void* recordBuffer, void* pageBuffer) {
+        unsigned short insertStartOffset = getInsertStartOffset(pageBuffer);
+
+        return true;
     }
 
 } // namespace PeterDB
